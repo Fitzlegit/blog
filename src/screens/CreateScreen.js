903aea746599915fb78b52a0b96bf44adc459473
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-06-06 22:21:46 -0400

    addec callback function to return user to to index

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from 'react';
 import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 import { Context } from '../context/BlogContext';
 
-const CreateScreen = () => {
+const CreateScreen = ({ navigation }) => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const { addBlogPost } = useContext(Context);
@@ -26,7 +26,10 @@ const CreateScreen = () => {
         <Button
           title="Add Blog Post"
           color="black"
-          onPress={() => addBlogPost(title, content)}
+          onPress={() => {addBlogPost(title, content, () => {
+            navigation.navigate('Index');
+            });
+          }}
          />
     </View>
   )
