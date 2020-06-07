33943f817ef309f3c70bedbaae7b29e4f88cd2e2
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-06-06 20:59:20 -0400

    initial commit

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const CreateScreen = () => {
+  return (
+    <View>
+      <Text>Create Screen</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+
+})
+
+
+export default CreateScreen;
