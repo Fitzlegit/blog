39c48b75daee9838b2cd7c2e0e50df283a8c2d1f
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-06-06 22:09:19 -0400

    styled input and labels

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,15 +1,53 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useContext, useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import { Context } from '../context/BlogContext';
 
 const CreateScreen = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const { addBlogPost } = useContext(Context);
+
   return (
-    <View>
-      <Text>Create Screen</Text>
+    <View style={styles.viewStyle}>
+      <Text style={styles.labelStyle}>Title:</Text>
+      <TextInput
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+        style={styles.inputStyle}
+        />
+      <Text style={styles.labelStyle}>Content:</Text>
+      <TextInput
+        multiline
+        numberOfLines={5}
+        value={content}
+        onChangeText={(text) => setContent(text)}
+        style={styles.inputStyle}
+        />
+        <Button
+          title="Add Blog Post"
+          color="black"
+          onPress={() => addBlogPost(title, content)}
+         />
     </View>
   )
 };
 
 const styles = StyleSheet.create({
+  viewStyle: {
+    marginHorizontal: 10,
+    justifyContent: 'space-between'
+  },
+  inputStyle: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: 'black',
+    textAlignVertical: 'top',
+    marginBottom: 10,
+  },
+  labelStyle: {
+    fontSize: 20,
+    marginBottom: 5
+  },
 
 })
 
