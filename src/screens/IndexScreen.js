ff5a7ac35bd8a8c54503a8d4eda805fee676b5b7
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-06-06 22:11:02 -0400

    added icon to header

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -30,6 +30,14 @@ const IndexScreen = ({ navigation }) => {
   )
 };
 
+IndexScreen.navigationOptions = ({ navigation }) => {
+  return {
+      headerRight: () => <TouchableOpacity onPress={() => navigation.navigate('Create')}>
+      <Entypo name="plus" size={30} style={{ marginRight: 15 }} />
+      </TouchableOpacity>
+  };
+};
+
 const styles = StyleSheet.create({
   rowStyle: {
     flexDirection: 'row',
