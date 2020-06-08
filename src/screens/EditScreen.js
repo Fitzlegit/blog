9ffd2425d44a6688b2facc949372fe8d84816e34
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-06-07 23:18:43 -0400

    initial commit

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+
+const EditScreen = () => {
+  return (
+    <Text>Edit Screen</Text>
+  );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default EditScreen;
