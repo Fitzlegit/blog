313441e40bfc807eeb71f934adb158b55bcc20e0
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-06-08 20:03:27 -0400

    wired up edit screen

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
-import IndexScreen from './src/screens/IndexScreen';
-import {Provider} from './src/context/BlogContext';
-import ShowScreen from './src/screens/ShowScreen';
-import CreateScreen from './src/screens/CreateScreen';
-import EditScreen from './src/screens/CreateScreen';
+import React from "react";
+import {StyleSheet, Text, View} from "react-native";
+import {createAppContainer} from "react-navigation";
+import {createStackNavigator} from "react-navigation-stack";
+import IndexScreen from "./src/screens/IndexScreen";
+import {Provider} from "./src/context/BlogContext";
+import ShowScreen from "./src/screens/ShowScreen";
+import CreateScreen from "./src/screens/CreateScreen";
+import EditScreen from "./src/screens/EditScreen";
 
 const navigator = createStackNavigator({
   Index: IndexScreen,
@@ -14,16 +14,16 @@ const navigator = createStackNavigator({
   Create: CreateScreen,
   Edit: EditScreen
 }, {
-  initialRouteName: 'Index',
+  initialRouteName: "Index",
   defaultNavigationOptions: {
-    title: 'Blogs'
+    title: "Blogs"
   }
 });
 
 const App = createAppContainer(navigator);
 
 export default() => {
-  return <Provider>
+  return (<Provider>
     <App/>
-  </Provider>
+  </Provider>);
 };
