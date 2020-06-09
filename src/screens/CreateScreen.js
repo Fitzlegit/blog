a1f602dcef826db4812ae6c88f554341aa0b43b5
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-06-08 20:07:06 -0400

    refractored code and set as a container

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,58 +1,12 @@
-import React, { useContext, useState } from 'react';
-import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
-import { Context } from '../context/BlogContext';
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
+import { Context } from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
 
 const CreateScreen = ({ navigation }) => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
   const { addBlogPost } = useContext(Context);
-
-  return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.labelStyle}>Title:</Text>
-      <TextInput
-        value={title}
-        onChangeText={(text) => setTitle(text)}
-        style={styles.inputStyle}
-        />
-      <Text style={styles.labelStyle}>Content:</Text>
-      <TextInput
-        multiline
-        numberOfLines={5}
-        value={content}
-        onChangeText={(text) => setContent(text)}
-        style={styles.inputStyle}
-        />
-        <Button
-          title="Add Blog Post"
-          color="black"
-          onPress={() => {addBlogPost(title, content, () => {
-            navigation.navigate('Index');
-            });
-          }}
-         />
-    </View>
-  )
 };
 
-const styles = StyleSheet.create({
-  viewStyle: {
-    marginHorizontal: 10,
-    justifyContent: 'space-between'
-  },
-  inputStyle: {
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: 'black',
-    textAlignVertical: 'top',
-    marginBottom: 10,
-  },
-  labelStyle: {
-    fontSize: 20,
-    marginBottom: 5
-  },
-
-})
-
+const styles = StyleSheet.create({});
 
 export default CreateScreen;
