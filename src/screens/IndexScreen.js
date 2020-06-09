00a890edea18de51c414ee25407e1b356a860dba
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-06-09 10:00:17 -0400

    add useEffect hook to pervent infinite loop when making get request

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,58 +1,88 @@
-import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, TouchableOpacity } from 'react-native'
-import { Context } from '../context/BlogContext';
-import { Entypo } from '@expo/vector-icons'
-
+import React, { useContext, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { Context } from "../context/BlogContext";
+import { Entypo } from "@expo/vector-icons";
 
 const IndexScreen = ({ navigation }) => {
-  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
+  const { state, addBlogPost, deleteBlogPost, getBlogPosts } = useContext(
+    Context
+  );
+
+  useEffect(() => {
+    getBlogPosts();
+
+    navigation.addListener("didFocus", () => {
+      getBlogPosts();
+    });
+
+    return () => {
+      listener.remove();
+    };
+  }, []);
 
   return (
     <View>
       <FlatList
         data={state}
-        keyExtractor={blogPost => blogPost.title}
+        keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
           return (
-            <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("Show", { id: item.id })}
+            >
               <View style={styles.rowStyle}>
-                  <Text style={styles.titleStyle}>{item.title} - {item.id}</Text>
-                  <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-                    <Entypo style={styles.iconStyle} name="trash" />
-                  </TouchableOpacity>
+                <Text style={styles.titleStyle}>
+                  {item.title}- {item.id}
+                </Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <Entypo style={styles.iconStyle} name="trash" />
+                </TouchableOpacity>
               </View>
             </TouchableOpacity>
           );
         }}
       />
     </View>
-  )
+  );
 };
 
 IndexScreen.navigationOptions = ({ navigation }) => {
   return {
-      headerRight: () => <TouchableOpacity onPress={() => navigation.navigate('Create')}>
-      <Entypo name="plus" size={30} style={{ marginRight: 15 }} />
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate("Create")}>
+        <Entypo
+          name="plus"
+          size={30}
+          style={{
+            marginRight: 15,
+          }}
+        />
       </TouchableOpacity>
+    ),
   };
 };
 
 const styles = StyleSheet.create({
   rowStyle: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     paddingVertical: 20,
     borderTopWidth: 1,
-    borderColor: 'gray',
-    paddingHorizontal: 10
+    borderColor: "gray",
+    paddingHorizontal: 10,
   },
   titleStyle: {
     fontSize: 18,
   },
   iconStyle: {
-    fontSize: 24
-  }
-
+    fontSize: 24,
+  },
 });
 
 export default IndexScreen;
