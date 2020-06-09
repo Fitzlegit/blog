dab6f009093a96985dd0b8671c63cbe0fd980e28
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-06-08 20:20:04 -0400

    passed down callback function to onSubmit

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -5,6 +5,14 @@ import BlogPostForm from "../components/BlogPostForm";
 
 const CreateScreen = ({ navigation }) => {
   const { addBlogPost } = useContext(Context);
+
+  return (
+    <BlogPostForm
+      onSubmit={(title, content) => {
+        addBlogPost(title, content, () => navigation.navigate("Index"));
+      }}
+    />
+  );
 };
 
 const styles = StyleSheet.create({});
