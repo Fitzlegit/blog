d6b91e677d28fe6e93faf3572af8f3211cee3248
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-06-08 20:02:37 -0400

    initial commit

---
+++ src/components/BlogPostForm.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+
+const BlogPostForm = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  return (
+    <View style={styles.viewStyle}>
+      <Text style={styles.labelStyle}>Title:</Text>
+      <TextInput
+        value={title}
+        onChangeText={(text) => setTitle(text)}
+        style={styles.inputStyle}
+      />
+      <Text style={styles.labelStyle}>Content:</Text>
+      <TextInput
+        multiline
+        numberOfLines={5}
+        value={content}
+        onChangeText={(text) => setContent(text)}
+        style={styles.inputStyle}
+      />
+      <Button title="Save Blog Post" color="black" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  viewStyle: {
+    marginHorizontal: 10,
+    justifyContent: "space-between",
+  },
+  inputStyle: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    textAlignVertical: "top",
+    marginBottom: 10,
+  },
+  labelStyle: {
+    fontSize: 20,
+    marginBottom: 5,
+  },
+});
+
+export default BlogPostForm;
