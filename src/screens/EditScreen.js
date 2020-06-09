1439a543a0d34a041dd5628f145745a964f2fc24
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2020-06-08 20:07:33 -0400

    refractor code and set as container

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
+import { Context } from "../context/BlogContext";
+import BlogPostForm from "../components/BlogPostForm";
 
-const EditScreen = () => {
-  return (
-    <Text>Edit Screen</Text>
+const EditScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+
+  const blogPost = state.find(
+    (blogPost) => blogPost.id === navigation.getParam("id")
   );
+
+  return <BlogPostForm />;
 };
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 
 export default EditScreen;
